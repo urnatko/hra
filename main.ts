8665568557d080ee18d382da8c7f92e64b131758
urnatko <92942456+urnatko@users.noreply.github.com>
2021-10-24 10:28:07 +0200

    Aktualizovat main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,18 +2,29 @@ input.onButtonPressed(Button.A, function () {
     lod.change(LedSpriteProperty.X, -1)
 })
 input.onButtonPressed(Button.AB, function () {
-    let strela: game.LedSprite = null
-    let nepritel: game.LedSprite = null
-    if (nepritel.get(LedSpriteProperty.Y) == strela.get(LedSpriteProperty.Y)) {
+    if (strela.isTouching(nepritel)) {
         game.addScore(1)
+    } else {
+        for (let index = 0; index < 4; index++) {
+            strela.change(LedSpriteProperty.Y, -1)
+        }
     }
 })
 input.onButtonPressed(Button.B, function () {
     lod.change(LedSpriteProperty.X, 1)
 })
+let nepritel: game.LedSprite = null
+let strela: game.LedSprite = null
 let lod: game.LedSprite = null
 game.setScore(0)
 lod = game.createSprite(randint(0, 4), 4)
+strela = lod
+nepritel = game.createSprite(randint(0, 4), 0)
 basic.forever(function () {
-	
+    if (strela.isTouching(nepritel)) {
+        game.gameOver()
+    } else {
+        nepritel.change(LedSpriteProperty.Y, 1)
+        nepritel = game.createSprite(randint(0, 4), 0)
+    }
 })
